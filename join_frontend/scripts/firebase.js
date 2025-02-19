cfc7cb754275abdd38af1559e7b91ca040ce8a5e
Alexander Hardtke <5yl3kz@googlemail.com>
2025-02-19 15:07:20 +0100

    Entferne Datei aus dem Tracking

--- join_frontend/scripts/firebase.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfigHidden = {
-  apiKey: "AIzaSyA29i-_xi46h4ctXmpy97ZiCmkInm8bWsk",
-  authDomain: "join-357c8.firebaseapp.com",
-  databaseURL: "https://join-357c8-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "join-357c8",
-  storageBucket: "join-357c8.firebasestorage.app",
-  messagingSenderId: "360169850849",
-  appId: "1:360169850849:web:83977040e4445a77e134a0"
-};
