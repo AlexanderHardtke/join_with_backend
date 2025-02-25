6e5b71a4669f5fd6bcc4e96eab676c9865a293d2
Alexander Hardtke <5yl3kz@googlemail.com>
2025-02-25 09:13:48 +0100

    added first and last name to registration serializer

--- join_db/api/serializers.py
+++ join_db/api/serializers.py
@@ -33,7 +33,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
         if User.objects.filter(email=self.validated_data['email']).exists():
             raise serializers.ValidationError({'error':'email already used'})
 
-        account = User(email=self.validated_data['email'], username=self.validated_data['username'])
+        account = User(email=self.validated_data['email'], username=self.validated_data['username'], first_name=self.validated_data['first_name'], last_name=self.validated_data['last_name'],)
         account.set_password(pw)
         account.save()
         return account
